8059a9e2036913ba1667b12ccfd7d7d2ad373e8c
José Manuel Nieto Sánchez <superjmn@outlook.com>
2020-01-29 15:54:54 +0100

    Always materialize enumerable to list if it's bound to any UI control

--- SampleApp/SampleApp.Shared/ViewModels/SampleSectionViewModel.cs
+++ SampleApp/SampleApp.Shared/ViewModels/SampleSectionViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
 using System.Reactive.Linq;
-using System.Threading.Tasks;
 using AutoMapper;
 using ReactiveUI;
 using SampleApp.Infrastructure;
@@ -15,14 +14,14 @@ namespace SampleApp.ViewModels
     public class SampleSectionViewModel : ReactiveObject, IDisposable
     {
         private readonly ObservableAsPropertyHelper<bool> isBusy;
-        private readonly ObservableAsPropertyHelper<IEnumerable<ItemViewModel>> items;
+        private readonly ObservableAsPropertyHelper<List<ItemViewModel>> items;
         private readonly CompositeDisposable disposables = new CompositeDisposable();
 
         public SampleSectionViewModel(IMyService service, IMapper mapper, IDialogService dialogService)
         {
             Fetch = ReactiveCommand.CreateFromObservable(() =>
                 Observable.FromAsync(service.GetItems)
-                    .Select(list => list.Select(mapper.Map<ItemViewModel>)));
+                    .Select(list => list.Select(mapper.Map<ItemViewModel>).ToList()));
 
             ShowError = ReactiveCommand.CreateFromTask<Exception>(e => dialogService.Show("Error", $"There has been an error while fetching data from the service."));
             Fetch.ThrownExceptions.InvokeCommand(ShowError);
@@ -35,18 +34,11 @@ namespace SampleApp.ViewModels
 
         public ReactiveCommand<Exception, Unit> ShowError { get; set; }
 
-        private static async Task<IEnumerable<ItemViewModel>> Select(IMyService service, IMapper mapper)
-        {
-            var list = await service.GetItems();
-            var vms = list.Select(mapper.Map<ItemViewModel>);
-            return vms;
-        }
-
         public bool IsBusy => isBusy.Value;
 
-        public ReactiveCommand<Unit, IEnumerable<ItemViewModel>> Fetch { get; }
+        public ReactiveCommand<Unit, List<ItemViewModel>> Fetch { get; }
 
-        public IEnumerable<ItemViewModel> Items => items.Value;
+        public List<ItemViewModel> Items => items.Value;
 
         public void Dispose()
         {
